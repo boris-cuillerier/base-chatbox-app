f111428788ebbb2b2fb541e4711e8429116c1601
Aliocha <aliocha@MacBook-Air-de-Aliocha.local>
2025-08-08 13:19:04 +0200

    Form component creation

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import React, { Component } from 'react'
 import './App.css'
+import Form from './components/Form'
 
 class App extends Component {
   render () {
     return (
-      <div className='box' />
+      <div className='box'>
+        <div>
+          <div className='messages'>
+
+          </div>
+        </div>
+        <Form></Form>
+      </div>
+      
     )
   }
 }
