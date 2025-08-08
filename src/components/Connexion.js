5cdfebb023c2f7038b18b79701be809cbd49f7c8
Aliocha <aliocha@MacBook-Air-de-Aliocha.local>
2025-08-08 12:09:02 +0200

    Connexion form creation

--- src/components/Connexion.js
+++ src/components/Connexion.js
@@ -3,9 +3,18 @@ import React, { Component } from "react";
 class Connexion extends Component {
   render() {
     return (
-      <div>
-        
+      <div className="connexionBox">
+        <form className="connexion">
+            <input
+                placeholder="Pseudo"
+                type="text"
+                required
+            />
+            <button type="submit">GO</button>
+        </form>
       </div>
     );
   }
 }
+
+export default Connexion;
