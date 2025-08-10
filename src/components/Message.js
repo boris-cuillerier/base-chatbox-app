4015ca3e7a16375565522f2d2b40d85feca32781
Aliocha <aliocha@MacBook-Air-de-Aliocha.local>
2025-08-10 09:49:55 +0200

    Manage multi users

--- src/components/Message.js
+++ src/components/Message.js
@@ -1,11 +1,19 @@
 import React from "react"
 
-const Message = ({pseudo, message}) => {
-    return (
-        <p className="user-message">
-            {pseudo} : {message}
-        </p>
-    );
+const Message = ({pseudo, message, isUser}) => {
+    if (isUser(pseudo)) {
+        return (
+            <p className="user-message">
+                {pseudo} : {message}
+            </p>
+        );
+    } else {
+        return (
+            <p className="not-user-message">
+                {pseudo} : {message}
+            </p>
+        );
+    }
 };
 
 export default Message;
